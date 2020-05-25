717e5ed4bdf365d7e7a9308c669d839ce1fc414a
Sambhav Jain <52211108+Sambhavj821@users.noreply.github.com>
2020-05-25 23:34:28 +0530

    mlops

--- mlproject.py
+++ mlproject.py
@@ -127,11 +127,11 @@ scores = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', scores[0])
 print('Test accuracy:', scores[1])
 
-accuracy_file = open('/root/mlops/accuracy.txt','w')
+accuracy_file = open('mlops/accuracy.txt','w')
 accuracy_file.write(str(scores[1]))
 accuracy_file.close()
 
-display_matter = open('/root/mlops/display_matter.html','r+')
+display_matter = open('mlops/display_matter.html','r+')
 display_matter.read()
 display_matter.write('<pre>\n---------------------------------------------\n')
 display_matter.write(this_layer)
