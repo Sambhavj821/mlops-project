3c00de5d09a01c9d7f79b8e43aae33ecbad9d2b2
Sambhav Jain <52211108+Sambhavj821@users.noreply.github.com>
2020-05-25 23:12:22 +0530

    mlops

--- Success_mail.py
+++ Success_mail.py
@@ -1,9 +1,9 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-host_address = "***********.com"
-host_pass = "*************"
-guest_address = "***************@gmail.com"
+host_address = "Sambhavj821@gmail.com"
+host_pass = "Alka@242"
+guest_address = "Sambhavj821@gmail.com"
 subject = "Regarding Success of your model "
 content = '''Hello, 
 				Developer this is an email regarding to your last commit. Your last commit was taken into consideration and based on that the trained model has given best accuracy .
